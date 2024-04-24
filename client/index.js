bc3a58b8bbbe9211e35ce80c7f60f8d01fbe8c35
norrent yu <norrentyu@outlook.com>
2024-04-24 19:20:16 +0800

    First commit

--- client/index.js
+++ client/index.js
@@ -5,10 +5,15 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // prove to the server we're on the nice list
+  const nameToVerify = "Ross Schinner"
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === nameToVerify);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    nameToVerify,
   });
 
   console.log({ gift });
